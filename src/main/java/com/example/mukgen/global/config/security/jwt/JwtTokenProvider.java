88a1bf35eda973f09e2d341da288f4b8b72b5846
HyunSu1768 <azxcv1768@gmail.com>
2023-04-18 22:46:16 +0900

    FEAT: JWT, Security 관련 기능 추가

---
+++ src/main/java/com/example/mukgen/global/config/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,69 @@
+package com.example.mukgen.global.config.security.jwt;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+    private String secretKey = "mukgenprojectmukgenproject";
+
+    private Long tokenValidTime = 30 * 60 * 1000L;
+
+    private final UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    //JWT 토큰 생성
+    public String createToken(String userId) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("type","access")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .compact();
+    }
+
+    // JWT 토큰에서 인증 정보 조회
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+    }
+
+    //토큰에서 회원 정보 추출
+    public String getUserPk(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request){
+        return request.getHeader("Authorization");
+    }
+
+    public boolean validateToken(String jwtToken){
+        try{
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
