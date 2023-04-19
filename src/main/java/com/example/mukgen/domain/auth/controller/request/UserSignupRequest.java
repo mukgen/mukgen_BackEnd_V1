533ccc1a51e7ce0b5f778dbc34d437f1b04c8f6c
HyunSu1768 <azxcv1768@gmail.com>
2023-04-19 10:39:13 +0900

    FEAT: Validation 추가

--- src/main/java/com/example/mukgen/domain/auth/controller/request/UserSignupRequest.java
+++ src/main/java/com/example/mukgen/domain/auth/controller/request/UserSignupRequest.java
@@ -2,14 +2,22 @@ package com.example.mukgen.domain.auth.controller.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 public class UserSignupRequest {
 
+
+    @Size(max = 4, message = "이름은 최대 4자 입니다.")
     private String name;
 
+    @Size(max = 15, message = "아이디는 최대 15자 입니다.")
     private String userId;
-
+    @Pattern(regexp = "^(?=.*[!@#$%^&*])(?=.{1,20}$).*",
+            message = "비밀번호는 최대 20글자이고, 특수문자 한개가 포함되어야 합니다.")
     private String password;
 
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$")
     private String phoneNumber;
 }
