23b47c4e5b312bb1297c9fa719e624e9bfe8fd04
HyunSu1768 <azxcv1768@gmail.com>
2023-05-02 10:37:35 +0900

    ⚡️ :: 급식 조회 성능 개선

---
+++ src/main/java/com/example/mukgen/domain/meal/service/MealService.java
@@ -0,0 +1,47 @@
+package com.example.mukgen.domain.meal.service;
+
+
+import com.example.mukgen.domain.meal.entity.Rice;
+import com.example.mukgen.domain.meal.entity.RiceType;
+import com.example.mukgen.domain.meal.repository.MealRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MealService {
+
+    private final MealApi mealApi;
+
+    private final MealRepository mealRepository;
+
+    private final ConcurrentHashMap<Integer, Rice> riceCache = new ConcurrentHashMap<>();
+
+    @Transactional
+    public Rice findRice(RiceType riceType,int year,int month,int day){
+        int addId = 0;
+        if (riceType == RiceType.BREAKFAST) {
+            addId = 1;
+        } else if (riceType == RiceType.LUNCH) {
+            addId = 2;
+        } else if (riceType == RiceType.DINNER) {
+            addId = 3;
+        }
+        int id = (year * 10000 + month * 100 + day) * 10 + addId;
+        Rice rice = riceCache.get(id);
+        if (rice == null) {
+            if(!mealRepository.existsById(id)){
+                rice = mealApi.getRice(riceType, year, month, day);
+                mealRepository.save(rice);
+            }
+            riceCache.put(id, rice);
+        }
+
+        return rice;
+    }
+
+}
