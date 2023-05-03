d6d1cde16b605e91e273d71f7ef5b757b08c8251
HyunSu1768 <azxcv1768@gmail.com>
2023-05-03 10:35:08 +0900

    ♻️ :: MealRequest 추가

--- src/main/java/com/example/mukgen/domain/meal/controller/MealController.java
+++ src/main/java/com/example/mukgen/domain/meal/controller/MealController.java
@@ -1,5 +1,6 @@
 package com.example.mukgen.domain.meal.controller;
 
+import com.example.mukgen.domain.meal.entity.dto.request.MealRequest;
 import com.example.mukgen.domain.meal.service.MealApi;
 import com.example.mukgen.domain.meal.entity.Rice;
 import com.example.mukgen.domain.meal.entity.RiceType;
@@ -7,6 +8,7 @@ import com.example.mukgen.domain.meal.service.MealService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,14 +17,11 @@ public class MealController {
 
     private final MealService mealService;
 
-    @GetMapping("/meal/{riceType}/{year}/{month}/{day}")
+    @GetMapping("/meal")
     public Rice getMeal(
-            @PathVariable RiceType riceType,
-            @PathVariable int year,
-            @PathVariable int month,
-            @PathVariable int day
-    ){
-        return mealService.findRice(riceType,year,month,day);
+            @RequestBody MealRequest request
+            ){
+        return mealService.findRice(request);
     }
 
 }
--- src/main/java/com/example/mukgen/domain/meal/service/MealService.java
+++ src/main/java/com/example/mukgen/domain/meal/service/MealService.java
@@ -3,6 +3,7 @@ package com.example.mukgen.domain.meal.service;
 
 import com.example.mukgen.domain.meal.entity.Rice;
 import com.example.mukgen.domain.meal.entity.RiceType;
+import com.example.mukgen.domain.meal.entity.dto.request.MealRequest;
 import com.example.mukgen.domain.meal.repository.MealRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,22 +24,33 @@ public class MealService {
     private final ConcurrentHashMap<Integer, Rice> riceCache = new ConcurrentHashMap<>();
 
     @Transactional
-    public Rice findRice(RiceType riceType,int year,int month,int day){
-        int addId = 0;
-        if (riceType == RiceType.BREAKFAST) {
-            addId = 1;
-        } else if (riceType == RiceType.LUNCH) {
-            addId = 2;
-        } else if (riceType == RiceType.DINNER) {
-            addId = 3;
-        }
+    public Rice findRice(MealRequest request){
+
+        RiceType riceType = request.getRiceType();
+
+        int year = request.getYear();
+
+        int month = request.getMonth();
+
+        int day = request.getDay();
+
+        int addId = switch (riceType) {
+            case BREAKFAST -> 1;
+            case LUNCH -> 2;
+            case DINNER -> 3;
+        };
+
         int id = (year * 10000 + month * 100 + day) * 10 + addId;
+
         Rice rice = riceCache.get(id);
+
         if (rice == null) {
+
             if(!mealRepository.existsById(id)){
                 rice = mealApi.getRice(riceType, year, month, day);
                 mealRepository.save(rice);
             }
+
             else {
                 rice = mealRepository.findById(id)
                         .orElseThrow(()-> new EntityNotFoundException("찾을 수 없습니다."));
