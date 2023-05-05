262b14a96c342423dd3b1d8739d4ab8d1a9b7a20
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-05-06 00:27:22 +0900

    🔥 :: 커밋 실수로 남은 더미 파일 삭제

--- src/main/java/com/example/mukgen/domain/lunchboard/repository/LunchBoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mukgen.domain.lunchboard.repository;
-
-import com.example.mukgen.domain.lunchboard.entity.LunchBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LunchBoardRepository extends JpaRepository<LunchBoard, Long> {
-}
