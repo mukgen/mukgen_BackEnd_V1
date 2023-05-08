4a666013a3f2606fb518fe82e09d2b88ea929842
HyunSu1768 <azxcv1768@gmail.com>
2023-05-08 22:47:58 +0900

    ✨ #40 :: Valid Exception Handling 추가

--- src/main/java/com/example/mukgen/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/mukgen/global/error/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.example.mukgen.global.error.exception.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,16 @@ import javax.validation.ConstraintViolationException;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidateException(MethodArgumentNotValidException e){
+
+        ErrorCode errorCode = ErrorCode.BAD_REQUEST;
+        ErrorResponse response = ErrorResponse.of(errorCode, e.getFieldError().getDefaultMessage());
+
+       return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+    }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e){
@@ -35,8 +46,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
 
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+
         ErrorResponse response = ErrorResponse.of(errorCode, e.getMessage());
 
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
