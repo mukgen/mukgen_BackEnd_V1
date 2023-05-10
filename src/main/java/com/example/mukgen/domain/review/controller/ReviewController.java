6eb0cc6a782cebf13ab43a4d23f7dad35761817d
HyunSu1768 <azxcv1768@gmail.com>
2023-05-10 09:39:14 +0900

    ✨ #47 :: 리뷰 순위 조회 기능 추가

--- src/main/java/com/example/mukgen/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/mukgen/domain/review/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.mukgen.domain.review.controller;
 
 import com.example.mukgen.domain.review.controller.dto.request.ReviewCreateRequest;
+import com.example.mukgen.domain.review.controller.dto.response.ReviewRankResponse;
 import com.example.mukgen.domain.review.controller.dto.response.ReviewResponseList;
 import com.example.mukgen.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,11 @@ public class ReviewController {
             @PathVariable int mealId
     ){
         return reviewService.findReview(mealId);
+    }
+
+    @GetMapping("/rank")
+    public ReviewRankResponse reviewRankList(){
+        return reviewService.findRankReview();
     }
 
     @GetMapping
--- src/main/java/com/example/mukgen/domain/review/service/ReviewService.java
+++ src/main/java/com/example/mukgen/domain/review/service/ReviewService.java
@@ -1,5 +1,7 @@
 package com.example.mukgen.domain.review.service;
 
+import com.example.mukgen.domain.review.controller.dto.response.ReviewMinimumResponse;
+import com.example.mukgen.domain.review.controller.dto.response.ReviewRankResponse;
 import com.example.mukgen.domain.rice.entity.Rice;
 import com.example.mukgen.domain.rice.repository.RiceRepository;
 import com.example.mukgen.domain.review.entity.Review;
@@ -9,12 +11,19 @@ import com.example.mukgen.domain.review.controller.dto.response.ReviewResponseLi
 import com.example.mukgen.domain.review.repository.ReviewRepository;
 import com.example.mukgen.domain.rice.service.exception.MealNotFoundException;
 import com.example.mukgen.domain.review.service.exception.ReviewAlreadyExistsException;
+import com.example.mukgen.domain.user.entity.User;
+import com.example.mukgen.domain.user.repository.UserRepository;
 import com.example.mukgen.domain.user.service.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @Transactional(readOnly = true)
@@ -26,6 +35,10 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
 
     private final UserFacade userFacade;
+
+    private final UserRepository userRepository;
+
+    private final EntityManager entityManager;
 
     @Transactional
     public void addReview(
@@ -76,4 +89,20 @@ public class ReviewService {
                 .reviewResponseList(reviewResponseList)
                 .build();
     }
+
+    public ReviewRankResponse findRankReview(){
+
+        String jpql = "SELECT e FROM tbl_user e ORDER BY SIZE(e.reviewList) DESC";
+
+        List<User> userSortList = entityManager.createQuery(jpql, User.class).setMaxResults(3).getResultList();
+
+        List<ReviewMinimumResponse> reviewMinimumResponseList = userSortList.stream()
+                .map(ReviewMinimumResponse::of).toList();
+
+        return ReviewRankResponse.builder()
+                .reviewMinimumResponseList(reviewMinimumResponseList)
+                .build();
+    }
+
+
 }
