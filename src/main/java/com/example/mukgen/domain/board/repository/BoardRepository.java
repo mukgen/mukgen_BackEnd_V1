edd9ae7d37573d53b89a72e216a245ea88a72305
HyunSu1768 <azxcv1768@gmail.com>
2023-05-13 15:13:44 +0900

    ✨ #64 :: 내가 쓴 게시글 조회

--- src/main/java/com/example/mukgen/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/mukgen/domain/board/repository/BoardRepository.java
@@ -1,6 +1,7 @@
 package com.example.mukgen.domain.board.repository;
 
 import com.example.mukgen.domain.board.entity.Board;
+import com.example.mukgen.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,8 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<Board,Long> {
 
     List<Board> findAllByCreateAtGreaterThan(LocalDateTime dateTime);
+
+    List<Board> findAllByUser(User user);
 
     @Query(value = "SELECT * FROM tbl_board WHERE WEEK(create_at) = ?1 and is_deleted = false", nativeQuery = true)
     List<Board> findByWeek(int week);
