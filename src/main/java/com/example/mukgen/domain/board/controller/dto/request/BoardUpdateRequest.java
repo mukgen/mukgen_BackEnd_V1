1b8589d34e5f156af9413f7d0b64aad51c590654
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-05-13 22:25:43 +0900

    ✨#34 :: 급식 게시판 Validation 추가

--- src/main/java/com/example/mukgen/domain/board/controller/dto/request/BoardUpdateRequest.java
+++ src/main/java/com/example/mukgen/domain/board/controller/dto/request/BoardUpdateRequest.java
@@ -4,13 +4,16 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardUpdateRequest {
 
-
+    @Size(min = 1, max = 30, message = "글 제목은 최소 1자, 최대 30자 입니다")
     private String title;
 
+    @Size(min = 1, max = 300, message = "글 제목은 최소 1자, 최대 300자 입니다")
     private String content;
 
 }
