deadf3a89b9a6e2cdc2de57da4daa6444eb4086b
HyunSu1768 <azxcv1768@gmail.com>
2023-05-13 12:14:15 +0900

    ✨ #61 :: 게시글 댓글 Soft Delete 기능 추가

--- src/main/java/com/example/mukgen/domain/boardcomment/entity/BoardComment.java
+++ src/main/java/com/example/mukgen/domain/boardcomment/entity/BoardComment.java
@@ -2,30 +2,40 @@ package com.example.mukgen.domain.boardcomment.entity;
 
 import com.example.mukgen.domain.board.entity.Board;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity(name = "tbl_boardComment")
+@Entity(name = "tbl_board_comment")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@SQLDelete(sql = "UPDATE `tbl_board_comment` SET is_deleted = true where id = ?")
+@Where(clause = "is_deleted = false")
 public class BoardComment {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column(name = "content")
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 
+    @Column(name = "writer")
     private String writer;
 
+    @Column(name = "create_at")
     private LocalDateTime createAt;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
 
     public void updateBoardComment(String content){
         this.content = content;
