8e66d0b6e15764fe1370064df212e5c7fccdd804
HyunSu1768 <azxcv1768@gmail.com>
2023-05-14 22:18:15 +0900

    ♻️ :: 임포트 안되는 문제 해결

--- src/main/java/com/example/mukgen/global/config/FilterConfig.java
+++ src/main/java/com/example/mukgen/global/config/FilterConfig.java
@@ -8,8 +8,7 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.http.HttpServletResponse;
+import com.practice.board.global.security.jwt.JwtFilter;
 
 @RequiredArgsConstructor
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
@@ -20,7 +19,7 @@ public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilte
 
     @Override
     public void configure(HttpSecurity http) {
-        com.practice.board.global.security.jwt.JwtFilter jwtTokenFilter = new com.practice.board.global.security.jwt.JwtFilter(jwtTokenProvider);
+        JwtFilter jwtTokenFilter = new JwtFilter(jwtTokenProvider);
         GlobalExceptionFilter globalExceptionFilter = new GlobalExceptionFilter(objectMapper);
 
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
