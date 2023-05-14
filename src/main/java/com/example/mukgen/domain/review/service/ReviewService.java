eeb16c7c9d3a921a7b3bb1e49e926dd833243824
HyunSu1768 <azxcv1768@gmail.com>
2023-05-14 22:24:57 +0900

    ♻️ :: 불필요한 임포트 삭제

--- src/main/java/com/example/mukgen/domain/review/service/ReviewService.java
+++ src/main/java/com/example/mukgen/domain/review/service/ReviewService.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
