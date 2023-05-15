222ca913956eb87b2838055da94ae49883fd3e09
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-05-16 08:18:01 +0900

    ♻️ :: 누락되었던 validation 메세지 추가

--- src/main/java/com/example/mukgen/domain/deliveryparty/controller/dto/request/DeliveryPartyRequest.java
+++ src/main/java/com/example/mukgen/domain/deliveryparty/controller/dto/request/DeliveryPartyRequest.java
@@ -14,14 +14,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeliveryPartyRequest {
 
-    @Size(min = 2, max = 20)
+    @Size(min = 2, max = 20, message = "메뉴는 최소 2자, 최대 20자입니다. ")
     private String menu;
 
-    @Min(2)
-    @Max(10)
+    @Min(value = 2, message = "인원은 최소 2명입니다.")
+    @Max(value = 10, message = "인원은 최대 10명입니다.")
     private Integer participantNumber;
 
-    @Size(min = 2, max = 20)
+    @Size(min = 2, max = 20, message = "장소는 최소 2자, 최대 20자입니다.")
     private String place;
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
