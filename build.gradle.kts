7bac8708692271b1b2bf649e8f5096ce8d507acf
HyunSu1768 <azxcv1768@gmail.com>
2023-05-17 17:02:14 +0900

    📝 :: JDK 버전 17 다운그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_19
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
     compileOnly {
