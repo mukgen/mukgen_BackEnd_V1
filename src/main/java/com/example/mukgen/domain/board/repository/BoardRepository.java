598ec81ad67da75f057e0685018596cb11f6f019
HyunSu1768 <azxcv1768@gmail.com>
2023-05-20 11:31:09 +0900

    ✨ #97 :: 게시글 페이징 기능 추가

--- src/main/java/com/example/mukgen/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/mukgen/domain/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.mukgen.domain.board.repository;
 
 import com.example.mukgen.domain.board.entity.Board;
 import com.example.mukgen.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,10 +15,10 @@ import java.util.List;
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
 
-    List<Board> findAllByCreatedAtGreaterThan(LocalDateTime dateTime, Sort sort);
+    Page<Board> findAllByCreatedAtGreaterThan(LocalDateTime dateTime, Sort sort, Pageable pageable);
 
-    List<Board> findAllByUser(User user);
+    Page<Board> findAllByUser(User user, Pageable pageable);
 
     @Query(value = "SELECT * FROM tbl_board WHERE WEEK(created_at) = ?1 and is_deleted = false ORDER BY created_at DESC", nativeQuery = true)
-    List<Board> findByWeek(int week);
+    Page<Board> findByWeek(int week, Pageable pageable);
 }
