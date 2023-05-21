626887474a124cd82b2514beac3aa2ec26ae6482
HyunSu1768 <azxcv1768@gmail.com>
2023-05-21 19:38:57 +0900

    📝 #104 :: 스케줄링 활성화

--- src/main/java/com/example/mukgen/MukgenApplication.java
+++ src/main/java/com/example/mukgen/MukgenApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class MukgenApplication {
