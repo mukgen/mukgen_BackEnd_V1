3ae48eef73903cbbc70bd8df849a9c13352e562c
HyunSu1768 <azxcv1768@gmail.com>
2023-05-21 19:38:14 +0900

    ✨ #104 :: 만날 시간이 지난 배달파티 자동 삭제

--- src/main/java/com/example/mukgen/domain/deliveryparty/repository/DeliveryPartyRepository.java
+++ src/main/java/com/example/mukgen/domain/deliveryparty/repository/DeliveryPartyRepository.java
@@ -5,10 +5,15 @@ import com.example.mukgen.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface DeliveryPartyRepository extends JpaRepository<DeliveryParty, Long> {
 
     boolean existsByWriterAccountId(String writerAccountId);
 
     boolean existsByUserListContainsAndId(User user,Long deliveryPartyId);
+
+    List<DeliveryParty> findAllByMeetTimeBefore(LocalDateTime time);
 }
---
+++ src/main/java/com/example/mukgen/domain/deliveryparty/service/DeliveryPartyScheduledService.java
@@ -0,0 +1,28 @@
+package com.example.mukgen.domain.deliveryparty.service;
+
+import com.example.mukgen.domain.deliveryparty.entity.DeliveryParty;
+import com.example.mukgen.domain.deliveryparty.repository.DeliveryPartyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryPartyScheduledService {
+
+    private final DeliveryPartyRepository deliveryPartyRepository;
+
+    @Scheduled(fixedDelay = 600000) // 10분마다 만난 시간이 지난 배달파티 삭제
+    public void deleteDeliveryParties(){
+
+        LocalDateTime time = LocalDateTime.now();
+        List<DeliveryParty> deliveryParties = deliveryPartyRepository.findAllByMeetTimeBefore(time);
+        for(DeliveryParty deliveryParty : deliveryParties){
+            deliveryPartyRepository.delete(deliveryParty);
+        }
+    }
+
+}
