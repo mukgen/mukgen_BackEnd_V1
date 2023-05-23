535b298d6550d288014ad3d4f2e9a1adc99aaeaf
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-05-23 19:46:15 +0900

    ♻️ :: RequestMapping 카멜케이스를 하이픈으로 변경

--- src/main/java/com/example/mukgen/domain/board/controller/BoardCommentController.java
+++ src/main/java/com/example/mukgen/domain/board/controller/BoardCommentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/boardComment")
+@RequestMapping("/board-comment")
 @RequiredArgsConstructor
 public class BoardCommentController {
 
