710427298f2ec59c7de9fddfd2554132cdb765a9
HyunSu1768 <azxcv1768@gmail.com>
2023-06-09 10:39:10 +0900

    🔥 :: 배달파티 자동삭제 버그 수정

--- src/main/java/com/example/mukgen/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/mukgen/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.mukgen.domain.user.repository;
 
+import com.example.mukgen.domain.deliveryparty.entity.DeliveryParty;
 import com.example.mukgen.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,5 +13,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByAccountId(String userId);
 
     boolean existsByAccountId(String userId);
+
+    Optional<User> findByDeliveryParty(DeliveryParty deliveryParty);
 
 }
