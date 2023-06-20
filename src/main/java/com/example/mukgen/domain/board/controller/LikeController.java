3b7287d7e0c1bdf8fbf2ea7946ac952a373264f5
HyunSu1768 <azxcv1768@gmail.com>
2023-06-21 00:59:24 +0900

    ♻️ :: 클래스 이름 변경

--- src/main/java/com/example/mukgen/domain/board/controller/LikeController.java
+++ src/main/java/com/example/mukgen/domain/board/controller/LikeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/like")
 @RestController
-public class LikesController {
+public class LikeController {
 
     private final LikeService likeService;
 
