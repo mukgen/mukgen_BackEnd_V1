b68e7a2d8ce17e57fdef43d7883fdd047c97da78
HyunSu1768 <azxcv1768@gmail.com>
2023-06-22 16:53:55 +0900

    ➕ #195 :: 의존성 추가

---
+++ src/main/java/com/example/mukgen/infra/feign/client/Config.java
@@ -0,0 +1,15 @@
+package com.example.mukgen.infra.feign.client;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+
+}
