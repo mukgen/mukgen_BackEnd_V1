277316f795c725002ac7497a36b0873c7668da88
HyunSu1768 <azxcv1768@gmail.com>
2023-07-18 13:55:25 +0900

    ♻️ :: 통계 response 수정

--- src/main/java/com/example/mukgen/domain/mealsuggestion/controller/dto/response/MealStatusResponse.java
+++ src/main/java/com/example/mukgen/domain/mealsuggestion/controller/dto/response/MealStatusResponse.java
@@ -3,14 +3,14 @@ package com.example.mukgen.domain.mealsuggestion.controller.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Builder
 public class MealStatusResponse {
 
     private int totalCount;
 
-    private LocalDateTime suggestionDate;
+    private int month;
+
+    private int day;
 
 }
