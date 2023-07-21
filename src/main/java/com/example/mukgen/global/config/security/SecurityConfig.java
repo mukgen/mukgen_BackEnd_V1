06f1acec95ea0fc43cbdaa9fef06e0aa72ef1642
HyunSu1768 <azxcv1768@gmail.com>
2023-07-21 12:40:52 +0900

    Security 복원

--- src/main/java/com/example/mukgen/global/config/security/SecurityConfig.java
+++ src/main/java/com/example/mukgen/global/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.mukgen.global.config.security;
 
+import com.example.mukgen.global.config.FilterConfig;
 import com.example.mukgen.global.config.security.auth.CustomUserDetailService;
 import com.example.mukgen.global.config.security.jwt.JwtResolver;
 import com.example.mukgen.global.config.security.jwt.JwtTokenProvider;
@@ -7,9 +8,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +33,33 @@ public class SecurityConfig{
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .exceptionHandling()
 
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/chef/**").hasRole("CHEF")
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider, jwtResolver, objectMapper, userDetailService))
+
+                .and().build();
+    }
 
 }
