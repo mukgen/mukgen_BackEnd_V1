6d4042cc442962702a42751fc1da9b1cf3628fa6
HyunSu1768 <azxcv1768@gmail.com>
2023-08-21 08:16:19 +0900

    deploy test

--- src/main/java/com/example/mukgen/MukgenApplication.java
+++ src/main/java/com/example/mukgen/MukgenApplication.java
@@ -16,6 +16,7 @@ import java.util.TimeZone;
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class MukgenApplication {
+
     @PostConstruct
     public void started(){
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
